9fe58b7fbc1d3667d9df7a52ff25262c191a8d9b
jmtrs <jmtorres15@gmail.com>
2026-09-18 13:27:21 +0200

    test: add task-neutral executable B-full scaffold

---
+++ baselines/b-full/src/app/App.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@mui/material";
+
+export function App() {
+  return (
+    <Container component="main" sx={{ py: 4 }}>
+      <Typography component="h1" variant="h4">
+        Support Console
+      </Typography>
+    </Container>
+  );
+}
