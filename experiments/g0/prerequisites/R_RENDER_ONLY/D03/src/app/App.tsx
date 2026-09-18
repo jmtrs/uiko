a729f98a2d1af1bced923cbb82ce75bc5e0236ee
jmtrs <jmtorres15@gmail.com>
2026-09-18 14:41:40 +0200

    test: add D03 prerequisite for G0 mechanism controls

---
+++ experiments/g0/prerequisites/R_RENDER_ONLY/D03/src/app/App.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+import { CustomerListPage } from "../features/customers/CustomerListPage";
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/customers" element={<CustomerListPage />} />
+    </Routes>
+  );
+}
